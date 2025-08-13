eb861c5cbd4690c5021bc1605373cf448b324f85
pxxja <poojabhambid2004@gmail.com>
2025-08-13 11:33:18 +0530

    create model to add tour

---
+++ backend/models/tourModel.js
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+
+const addTourSchema= new mongoose.Schema({
+    tourName:{
+        type:String,
+        required:true
+    },
+    description:{
+        type:String,
+        required:true
+    },
+    image:{
+        type:String,
+        required:true
+    },
+    price:{
+        type:Number,
+        required:true
+    },
+    date:{
+        type:Number,
+        required:true
+    }
+})
+const addTourModel=mongoose.models.addTour||mongoose.model("addTour",addTourSchema);
+export default addTourModel
