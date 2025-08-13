fb3399bbecc4c9103f7eb15cbbd27ea03d3a9e1e
pxxja <poojabhambid2004@gmail.com>
2025-08-13 18:42:39 +0530

    add: implement addTour function to handle new tour creation

---
+++ backend/controllers/admin/tourController.js
@@ -0,0 +1,33 @@
+import {v2 as cloudinary} from 'cloudinary';
+import addTourModel from '../../models/tourModel.js';
+
+const addTour=async(req,res)=>{
+    try {
+        const {tourName,description,price,date}=req.body;
+        const image =req.file;
+
+        if(!image){
+           return  res.json({success:false,message:"Image is required"})
+        }
+
+        const uploadedImage=await cloudinary.uploader.upload(image.path,{folder:"tours"});
+
+        const newTour={
+            tourName,
+            description,
+            price,
+            image:uploadedImage.secure_url,
+            date : Date.now()
+        }
+
+        const tour=new addTourModel(newTour);
+        await tour.save();
+        res.json({success:true,message:"Tour added"})
+
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:error.message});
+    }
+}
+
+export {addTour} 
