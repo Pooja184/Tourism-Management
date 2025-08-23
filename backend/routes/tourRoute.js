3851d98114fa372940f3bc8744d099537b24cc3a
pxxja <poojabhambid2004@gmail.com>
2025-08-23 19:50:05 +0530

    create route for all tours

--- backend/routes/tourRoute.js
+++ backend/routes/tourRoute.js
@@ -1,12 +1,14 @@
 import express from "express";
 import upload from "../middlewares/multer.js"; // Import Multer middleware for image upload handling
 import adminAuth from "../middlewares/adminAuth.js";
-import { addTour, listTours } from "../controllers/admin/tourController.js";
+import { addTour, getAllTours, listTours } from "../controllers/admin/tourController.js";
 
 const tourRouter=express.Router();
 
+//admin
 tourRouter.post("/add",adminAuth,upload.single('image'),addTour);
 tourRouter.get("/list",adminAuth,listTours);
 
-
+// user
+tourRouter.get("/alltours",getAllTours)
 export default tourRouter;
