f27cea37f5effbec5c281c15adb2463e1004d013
pxxja <poojabhambid2004@gmail.com>
2025-08-23 20:26:10 +0530

    add reducer in store

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit'
 import userReducer from "../features/user/userSlice.js";
 import adminReducer from "../features/admin/adminSlice.js";
 import tourReducer from "../features/admin/addToursSlice.js";
+import allToursReducer from "../features/user/tours.js";
 
 export const store = configureStore({
   reducer: {
     user:userReducer,
     admin:adminReducer,
-    tour:tourReducer
+    tour:tourReducer,
+    allTours:allToursReducer,
   },
 })
