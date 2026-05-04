0c1a90e6bb06e92eb818f36c5109235723f98a51
pxxja <poojabhambid2004@gmail.com>
2026-05-04 20:10:05 +0530

    feat: enhance HomeAdmin layout with gradient background and improved spacing

--- frontend/src/pages/admin/Home.jsx
+++ frontend/src/pages/admin/Home.jsx
@@ -1,17 +1,16 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
-import Sidebar from './Sidebar'
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Sidebar from "./Sidebar";
 
 const HomeAdmin = () => {
   return (
-    <div className="flex">
+    <div className="flex min-h-screen bg-[linear-gradient(180deg,#f7f3ea_0%,#eef7f2_100%)] text-slate-900">
       <Sidebar />
-      <div className="flex-1 p-6">
-        {/* Child routes will render here */}
+      <div className="min-w-0 flex-1 p-4 pt-20 md:p-8 md:pt-8">
         <Outlet />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default HomeAdmin
+export default HomeAdmin;
